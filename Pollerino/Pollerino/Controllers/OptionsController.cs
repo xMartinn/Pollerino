bc384debf01b6eee302d6e486c2e83538070bbb0
xMartinn <martinpipis@gmail.com>
2015-06-30 17:10:50 +0200

    Generated controlers - entity framework

---
+++ Pollerino/Pollerino/Controllers/OptionsController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Pollerino.Models;
+
+namespace Pollerino.Controllers
+{
+    public class OptionsController : Controller
+    {
+        private ModelContext db = new ModelContext();
+
+        // GET: Options
+        public ActionResult Index()
+        {
+            var options = db.Options.Include(o => o.Poll);
+            return View(options.ToList());
+        }
+
+        // GET: Options/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Option option = db.Options.Find(id);
+            if (option == null)
+            {
+                return HttpNotFound();
+            }
+            return View(option);
+        }
+
+        // GET: Options/Create
+        public ActionResult Create()
+        {
+            ViewBag.PollId = new SelectList(db.Polls, "PollId", "Link");
+            return View();
+        }
+
+        // POST: Options/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "OptionId,OptionText,PollId")] Option option)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Options.Add(option);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.PollId = new SelectList(db.Polls, "PollId", "Link", option.PollId);
+            return View(option);
+        }
+
+        // GET: Options/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Option option = db.Options.Find(id);
+            if (option == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.PollId = new SelectList(db.Polls, "PollId", "Link", option.PollId);
+            return View(option);
+        }
+
+        // POST: Options/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "OptionId,OptionText,PollId")] Option option)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(option).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.PollId = new SelectList(db.Polls, "PollId", "Link", option.PollId);
+            return View(option);
+        }
+
+        // GET: Options/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Option option = db.Options.Find(id);
+            if (option == null)
+            {
+                return HttpNotFound();
+            }
+            return View(option);
+        }
+
+        // POST: Options/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Option option = db.Options.Find(id);
+            db.Options.Remove(option);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Pollerino/Pollerino/Controllers/PollsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Pollerino.Models;
+
+namespace Pollerino.Controllers
+{
+    public class PollsController : Controller
+    {
+        private ModelContext db = new ModelContext();
+
+        // GET: Polls
+        public ActionResult Index()
+        {
+            return View(db.Polls.ToList());
+        }
+
+        // GET: Polls/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Poll poll = db.Polls.Find(id);
+            if (poll == null)
+            {
+                return HttpNotFound();
+            }
+            return View(poll);
+        }
+
+        // GET: Polls/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Polls/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "PollId,Link,QuestionText,MultipleChoices")] Poll poll)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Polls.Add(poll);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(poll);
+        }
+
+        // GET: Polls/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Poll poll = db.Polls.Find(id);
+            if (poll == null)
+            {
+                return HttpNotFound();
+            }
+            return View(poll);
+        }
+
+        // POST: Polls/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "PollId,Link,QuestionText,MultipleChoices")] Poll poll)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(poll).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(poll);
+        }
+
+        // GET: Polls/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Poll poll = db.Polls.Find(id);
+            if (poll == null)
+            {
+                return HttpNotFound();
+            }
+            return View(poll);
+        }
+
+        // POST: Polls/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Poll poll = db.Polls.Find(id);
+            db.Polls.Remove(poll);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
