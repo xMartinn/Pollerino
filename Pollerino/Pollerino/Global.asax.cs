2e00d0c91000333f74441346762d6a72a33d92ea
xMartinn <martinpipis@gmail.com>
2015-06-30 17:15:10 +0200

    EntityFramework stuff

--- Pollerino/Pollerino/Global.asax.cs
+++ Pollerino/Pollerino/Global.asax.cs
@@ -5,6 +5,8 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using Pollerino.Models;
+using System.Data.Entity;
 
 namespace Pollerino
 {
@@ -12,6 +14,9 @@ namespace Pollerino
     {
         protected void Application_Start()
         {
+
+            Database.SetInitializer<ModelContext>(new ModelInitializer());
+
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
