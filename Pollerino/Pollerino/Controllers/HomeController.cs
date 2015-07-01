7275bb00fe39766cbcaa2ee209a8a1e9702bdd5c
xMartinn <martinpipis@gmail.com>
2015-07-02 00:17:07 +0200

    controller update

--- Pollerino/Pollerino/Controllers/HomeController.cs
+++ Pollerino/Pollerino/Controllers/HomeController.cs
@@ -4,21 +4,63 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
+using Pollerino.Models;
+
 namespace Pollerino.Controllers
 {
     public class HomeController : Controller
     {
+        private ModelContext db = new ModelContext();
+
         public ActionResult Index()
         {
             return View();
         }
 
-        public ActionResult About()
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index(Poll poll)
         {
-            ViewBag.Message = "Your application description page.";
+
+            if (ModelState.IsValid)
+            {
+                db.Polls.Add(poll);
+                db.SaveChanges();
+
+                foreach (var option in poll.Options) {
+                    if (!String.IsNullOrWhiteSpace(option.OptionText))
+                    {
+                        option.WasChecked = false;
+                        option.PollId = poll.PollId;
+                        db.Options.Add(option);
+                    }
+                }
+                db.SaveChanges();
+ 
+                return RedirectToAction("Index");
+            }
+
 
             return View();
         }
+
+        public ActionResult About(int? id)
+        {
+            Poll poll= db.Polls.Find(id);
+            poll.Options = db.Options.Where(x => x.PollId == poll.PollId);  //should load auto - virtual 
+            return View(poll);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult About()
+        {
+
+            return View();
+        }
+
+
 
         public ActionResult Contact()
         {
