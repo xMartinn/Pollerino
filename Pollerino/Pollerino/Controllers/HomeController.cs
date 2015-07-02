30e30e51d3263687e8116b5937ba4dca398b80be
xMartinn <martinpipis@gmail.com>
2015-07-03 00:12:36 +0200

    Fixed controller

--- Pollerino/Pollerino/Controllers/HomeController.cs
+++ Pollerino/Pollerino/Controllers/HomeController.cs
@@ -17,7 +17,6 @@ namespace Pollerino.Controllers
             return View();
         }
 
-
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Index(Poll poll)
@@ -28,27 +27,33 @@ namespace Pollerino.Controllers
                 db.Polls.Add(poll);
                 db.SaveChanges();
 
-              /*  foreach (var option in poll.Options) {
-                    if (!String.IsNullOrWhiteSpace(option.OptionText))
-                    {
-                        option.WasChecked = false;
-                        option.PollId = poll.PollId;
-                        db.Options.Add(option);
-                    }
-                }
-                db.SaveChanges();
- */
-                return RedirectToAction("Index");
-            }
+                /*  foreach (var option in poll.Options) {
+                      if (!String.IsNullOrWhiteSpace(option.OptionText))
+                      {
+                          option.WasChecked = false;
+                          option.PollId = poll.PollId;
+                          db.Options.Add(option);
+                      }
+                  }
+                  db.SaveChanges();
+   */
+                return RedirectToAction("About", new { id = poll.PollId });
 
+            }
 
             return View();
         }
 
         public ActionResult About(int? id)
         {
-            Poll poll= db.Polls.Find(id);
-         //   poll.Options = db.Options.Where(x => x.PollId == poll.PollId).ToList();  //should load auto - virtual 
+
+            Poll poll = db.Polls.Find(id);
+            if (poll == null)
+            {
+                return Redirect("404");
+            }
+
+            poll.Options = db.Options.Where(x => x.PollId == poll.PollId).ToList();  //should load auto - virtual 
             return View(poll);
         }
 
@@ -56,17 +61,65 @@ namespace Pollerino.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult About(Poll poll)
         {
+            /*
+            if (db.Votes.Where(x => x.PollId == poll.PollId && x.VoterIp == System.Web.HttpContext.Current.Request.ServerVariables["REMOTE_ADDR"]).Count() > 0)
+            {
+                //TODO - person already voted angrytoken
+            }
+            */
+            if (poll.MultipleChoices)
+            {
+                foreach (var option in poll.Options)
+                {
+                    if (option.WasChecked)
+                    {
+                        Vote vote = new Vote();
+                        vote.VoterIp = System.Web.HttpContext.Current.Request.ServerVariables["REMOTE_ADDR"];
+                        // or HttpContext.Current.Request.UserHostAddress;
+                        vote.PollId = option.PollId;
+                        vote.OptionId = option.OptionId;
+                        db.Votes.Add(vote);
 
-            return View();
+                        //pripocitat vote Options.numVotes
+
+
+                        Option optionToUdt = db.Options.Find(vote.OptionId);
+                        optionToUdt.NumVotes += 1;
+                        db.SaveChanges();
+
+
+                    }
+                }
+            }
+            else
+            {
+                Vote vote = new Vote();
+                vote.VoterIp = System.Web.HttpContext.Current.Request.ServerVariables["REMOTE_ADDR"];
+                // or HttpContext.Current.Request.UserHostAddress;
+                vote.PollId = poll.PollId;
+                vote.OptionId = (int)poll.SelectedItem;
+                db.Votes.Add(vote);
+
+                //pripocitat vote Options.numVotes
+                Option optionToUdt = db.Options.Find(vote.OptionId);
+                optionToUdt.NumVotes += 1;
+                db.SaveChanges();
+            }
+
+            return RedirectToAction("Contact", new { id = poll.PollId });
         }
 
 
-
-        public ActionResult Contact()
+        public ActionResult Contact(int? id)
         {
-            ViewBag.Message = "Your contact page.";
+            Poll poll = db.Polls.Find(id);
 
-            return View();
+            if (poll == null)
+            {
+                return Redirect("404");
+            }
+            return View(poll);
         }
+
     }
 }
