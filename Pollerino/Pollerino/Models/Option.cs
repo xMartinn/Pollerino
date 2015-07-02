6c01359f1456d7a56cb263ca3777321d044e1faa
xMartinn <martinpipis@gmail.com>
2015-07-02 10:00:37 +0200

    Updated models - radiobuttons + checkboxes

--- Pollerino/Pollerino/Models/Option.cs
+++ Pollerino/Pollerino/Models/Option.cs
@@ -9,9 +9,9 @@ namespace Pollerino.Models
     {
         public int OptionId { get; set; }
         public string OptionText { get; set; }
-        public bool WasChecked { get; set; }
+        public bool WasChecked { get; set; } //just for checkbuttons
 
-        public virtual IEnumerable<Vote> Votes{get; set;}
+        public virtual List<Vote> Votes{get; set;}
 
         public int PollId { get; set; }
         public virtual Poll Poll { get; set; }
--- Pollerino/Pollerino/Models/Poll.cs
+++ Pollerino/Pollerino/Models/Poll.cs
@@ -8,11 +8,12 @@ namespace Pollerino.Models
     public class Poll
     {
        public int PollId { get; set; }
-       public string Link { get; set; }
        public string QuestionText { get; set; }
        public bool MultipleChoices { get; set; }
 
-       public virtual IEnumerable<Option> Options { get; set; }
+       public virtual List<Option> Options { get; set; }
+
+       public int? SelectedItem { get; set; } //just for radiobuttons
 
     }
 }
